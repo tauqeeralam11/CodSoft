6c737bb0974e5e50c000ea8fad1c19b687bc6149
Tauqeer Alam <153744945+tauqeeralam11@users.noreply.github.com>
2024-01-08 19:02:02 +0530

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,5 @@
 import string
 import random
-import time
 weak_password = string.ascii_lowercase
 moderate_password = string.ascii_lowercase+string.ascii_uppercase+string.digits
 strong_password = string.ascii_letters+string.digits+string.punctuation
@@ -8,8 +7,8 @@ while True:
     a = "WELCOME TO THE PASSWORD GENERATOR"
     b = a.center(50,"-")
     print(b,"\n")
-    gen = print("1.Generate A New Password")
-    exit = print("2.Exit")
+    print("1.Generate A New Password")
+    print("2.Exit")
     x = int(input("Enter Your Choice(1-2) : "))
     if x==1:
         length = int(input("Please Enter The Length Of Password : "))
@@ -22,23 +21,28 @@ while True:
             password = "".join(random.choices(weak_password,k=length))
             print("The Generated Password Is : ")
             print(password,"\n")
-            time.sleep(2)
+            q = input("<--Press Enter-->")
+            continue
         elif complexity_user==2:
             password1 = "".join(random.choices(moderate_password,k=length))
             print("The Generated Password Is : ")
             print(password1,"\n")
-            time.sleep(2)
+            q = input("<--Press Enter-->")
+            continue
         elif complexity_user==3:
             password2 = "".join(random.choices(strong_password,k=length))
             print("The Generated Password Is : ")
             print(password2,"\n")
-            time.sleep(2)
-            
+            q = input("<--Press Enter-->")
+            continue
         else:
-            print("Invalid Choice")
+            print("Invalid Choice. Please Try Again")
+            q = input("<--Press Enter-->")
+            continue
     elif x==2:
         print("Thanks For Using")
         break
     else:
-        print("Invalid Choice")
-        break
+        print("Invalid Choice. Please Try Again")
+        q = input("<--Press Enter-->")
+        continue
